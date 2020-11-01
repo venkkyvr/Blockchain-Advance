b507b9df314d27a932f65fddf1a06269725f16fe
venkkyvr <68055031+venkkyvr@users.noreply.github.com>
2020-11-02 00:47:17 +0530

    Add files via upload

---
+++ Day 4/check_int256_gas.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.5.13 < 0.7.3;
+
+contract checkint256gas{
+    
+    int256 a = 0;
+    
+    /*
+    
+        status	0x1 Transaction mined and execution succeed
+        transaction hash	0xfeeddd057c97b31653dc67315d7993e239ac8ad77359d282714d6f02a56c52ee 
+        contract address	0x358aa13c52544eccef6b0add0f801012adad5ee3 
+        from	0x5b38da6a701c568545dcfcb03fcb875f56beddc4 
+        to	checkint256gas.(constructor) 
+        gas	3000000 gas 
+        transaction cost	72552 gas 
+        execution cost	13472 gas
+    
+    */
+
+}
---
+++ Day 4/check_int8_gas.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.5.13 < 0.7.3;
+
+contract checkint8gas{
+    
+    int8 a = 0;
+    
+    /*
+    
+        status	0x1 Transaction mined and execution succeed
+        transaction hash	0x367a09f3bec793b4f2800a5e4f26944d90e01eac6d9069f04a41225a199adc27 
+        contract address	0xda0bab807633f07f013f94dd0e6a4f96f8742b53 
+        from	0x5b38da6a701c568545dcfcb03fcb875f56beddc4 
+        to	checkint8gas.(constructor) 
+        gas	3000000 gas 
+        transaction cost 74997 gas 
+        execution cost	14341 gas 
+    
+    */
+
+}
